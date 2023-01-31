0f5c5289a802657a61198fb634dfcd27b67f5730
Laura Pájaro <lauratecno30@gmail.com>
2023-01-31 20:13:03 -0300

    First commit. Version 1.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './imagenes/origami-yellow.png';
+import ListaDeTareas from './componentes/ListaDeTareas';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="aplicacion-tareas">
+      <div className='logo-contenedor'>
+        <img
+          src={logo}
+          alt='Imagen Logo'
+          className='logo' 
+          />
+      </div>
+      <div className='lista-tareas-principal'>
+        <h1>Mis tareas</h1>
+        <ListaDeTareas/>
+      </div>
     </div>
   );
 }
-
 export default App;
